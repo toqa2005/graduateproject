296b0fb3db61637e448e6dcc566dce5f4606b584
sayed <toqasayed,ostafa@gmail.com>
2026-09-03 15:09:19 +0300

    add file for data onboarding add file for texts onboarding add file for data model onboarding add images

---
+++ lib/utils/onboardingmodel.dart
@@ -0,0 +1,11 @@
+class OnBoardingModel {
+  final String image;
+  final String title;
+  final String body;
+
+  const OnBoardingModel({
+    required this.image,
+    required this.title,
+    required this.body,
+  });
+}
